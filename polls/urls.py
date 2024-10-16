90bed934675f52de1d6b83ea7ca12ed4cb0d4830
Programmist <niko4pok@gmail.com>
2024-10-16 10:30:48 +0300

    urls changed

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='Index'),
+    path('', views.index, name='index'),
     path('cartridges/', views.cartridges_list, name='cartridges_list'),
     path('cartridge/<int:cartridge_id>/', views.cartridge_detail, name='cartridge_detail'),
 ]
