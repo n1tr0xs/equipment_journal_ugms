e08b6eb1cb44db8b931a190357d4d909ecde7953
Programmist <niko4pok@gmail.com>
2024-10-16 10:04:59 +0300

    views created

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('cartridges/', views.cartridges_list, name='cartridges_list'),
+    path('cartridge/<int:cartridge_id>/', views.cartridge_detail, name='cartridge_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+
+from .models import Cartridge
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is index page.")
+    return render(request, 'polls/base.html')
+
+
+def cartridges_list(request):
+    cartridges = Cartridge.objects.order_by('id')
+    context = {
+        'cartridges_list': cartridges,
+    }
+    return render(request, 'polls/cartridges.html', context)
+
+
+def cartridge_detail(request, cartridge_id):
+    cartridge = get_object_or_404(Cartridge, pk=cartridge_id)
+    return render(request, 'polls/cartridge.html', {'cartridge': cartridge})
