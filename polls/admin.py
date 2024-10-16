f4b02194d021cecaa3cc0228c1a9e67bfd7a3926
Programmist <niko4pok@gmail.com>
2024-10-16 14:04:28 +0300

    list_display fixed

--- polls/admin.py
+++ polls/admin.py
@@ -3,20 +3,6 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(PeripheralType)
-class PeripheralTypeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
-    list_filter = []
-    search_fields = ['name']
-
-
-@admin.register(ComputerConfiguration)
-class ComputerConfiguration(admin.ModelAdmin):
-    list_display = ['id', 'name', 'processor', 'ram', 'motherboard', 'drives', 'power_supply']
-    list_filter = []
-    search_fields = ['name']
-
-
 @admin.register(Structure)
 class StructureAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'physical_place']
@@ -38,66 +24,80 @@ class WorksiteAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+@admin.register(PeripheralType)
+class PeripheralTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = []
+    search_fields = ['name']
+
+
+@admin.register(ComputerConfiguration)
+class ComputerConfiguration(admin.ModelAdmin):
+    list_display = ['id', 'name', 'processor', 'ram', 'motherboard', 'drives', 'power_supply']
+    list_filter = []
+    search_fields = ['name']
+
+
 @admin.register(Peripheral)
 class PeripheralAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'peripheral_type', 'worksite', 'technical_condition', 'disabling_reason']
+    list_display = ['id', 'name', 'peripheral_type', 'worksite', 'technical_condition', 'disabling_reason', 'inventory_number', 'serial_number']
     list_filter = ['peripheral_type', 'worksite', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(NetworkEquipment)
 class NetworkEquipmentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'structure', 'technical_condition', 'disabling_reason', 'ip_address']
+    list_display = ['id', 'name', 'structure', 'technical_condition', 'disabling_reason', 'ip_address', 'inventory_number', 'serial_number']
     list_filter = ['structure', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(Computer)
 class ComputerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'configuration', 'worksite', 'technical_condition', 'disabling_reason']
+    list_display = ['id', 'name', 'configuration', 'worksite', 'technical_condition', 'disabling_reason', 'inventory_number', 'serial_number']
     list_filter = ['configuration', 'worksite', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(Monitor)
 class MonitorAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason']
+    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason', 'inventory_number', 'serial_number']
     list_filter = ['worksite', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(MFP)
 class MFPAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason']
+    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason', 'inventory_number', 'serial_number']
     list_filter = ['worksite', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(UPS)
 class UPSAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason']
+    list_display = ['id', 'name', 'worksite', 'technical_condition', 'disabling_reason', 'inventory_number', 'serial_number']
     list_filter = ['worksite', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(MeteoUnit)
 class MeteoUnitAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'structure', 'technical_condition', 'disabling_reason', 'verification_date']
+    list_display = ['id', 'name', 'structure', 'technical_condition', 'disabling_reason', 'verification_date', 'inventory_number', 'serial_number']
     list_filter = ['structure', 'technical_condition']
     search_fields = ['name']
 
 
 @admin.register(Server)
 class ServerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'purpose', 'structure', 'technical_condition', 'disabling_reason', 'ip_address']
+    list_display = ['id', 'name', 'purpose', 'structure', 'technical_condition', 'disabling_reason', 'ip_address', 'inventory_number', 'serial_number']
     list_filter = ['structure', 'technical_condition']
     search_fields = ['name', 'purpose']
 
 
 @admin.register(Cartridge)
 class CartridgeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'number', 'mfp', 'technical_condition', 'disabling_reason']
-    list_filter = ['technical_condition']
+    list_display = ['id', 'name', 'number', 'mfp', 'technical_condition', 'disabling_reason', 'refills']
+    list_filter = ['technical_condition', 'refills']
     search_fields = ['name']
 
 
