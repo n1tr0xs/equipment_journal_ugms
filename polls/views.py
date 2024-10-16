fe4d6be4ffb4c75efaf0c3941d01b069ee7f62e1
Programmist <niko4pok@gmail.com>
2024-10-16 12:40:26 +0300

    cartridgelistview obsolete code removed

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,6 @@ class CartridgeListView(ListView):
     model = Cartridge
     context_object_name = 'cartridge_list'
     template_name = 'polls/cartridge_list.html'
-    queryset = Cartridge.objects.order_by('id')
 
 
 class CartridgeDetailView(DetailView):
