aaeefb88d48fde164eea24a9b0ed201daa0bb3f6
Programmist <niko4pok@gmail.com>
2024-10-24 11:21:21 +0300

    debug-deplot, header fix

---
+++ START_SERVER.bat
@@ -0,0 +1,5 @@
+@echo off
+
+manage.py runserver 10.55.0.100:2410
+
+pause
--- ugmseqjour/settings.py
+++ ugmseqjour/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(=$)b%b#ov1@v!h!34b=!p1@y*&s-2153h6ad8^^f-9it#2f$a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '10.55.0.100']
 
 
 # Application definition
